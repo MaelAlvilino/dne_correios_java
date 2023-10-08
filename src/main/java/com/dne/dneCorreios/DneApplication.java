8d17031e38c2b157ffc6e583eca53059adaa3dcb
MaelAlvilino <emmanuel.alvi@hotmail.com>
2023-10-08 16:13:11 -0300

    initial commit

---
+++ src/main/java/com/dne/dneCorreios/DneApplication.java
@@ -0,0 +1,13 @@
+package com.dne.dneCorreios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dne/dneCorreios/DneCorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dne.dneCorreios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DneCorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
